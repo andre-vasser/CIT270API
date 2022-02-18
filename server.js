4287c4ded356b2e85af755f2895aa8da01ba2517
Andre <vas17001@byui.edu>
2022-02-18 13:35:33 -0700

    This week we finished the certificate.

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const bodyParser = require('body-parser');
 const port = 3000;
 const md5 = require('md5')
 const app = express();
-const https = require ('https')
+const https = require ('https');
+const fs = require('fs');
 app.use(bodyParser.json());
 app.get('/', (req, res)=>{
     res.send("Hello HTTPS!")
 });
-https.createServer({},app).listen(3000,()=>{
+https.createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert'),
+    passphrase:"P@ssw0rd"
+},app).listen(port,()=>{
     console.log('listening...')
-})
 })
 app.post('/login', (req,res)=> {
     if(req.body.userName =="andre@gmail.com" && md5(req.body.password) =="29e25a521b2db5ab06dbd093886f08e8"){
@@ -19,5 +23,5 @@ app.post('/login', (req,res)=> {
         res.send("Who are you?")
     }
 });
-app.listen(port, ()=>{});
+
 //note
